cf54766084d22fc18ee51a2c513b5baf3bab9c25
ejshin-muhayu <ejshin@muhayu.com>
2024-08-15 20:27:41 +0900

    refactor: 첫 납부일이 있는 경우 계산하는 로직 메서드로 분리

--- src/test/java/com/example/starttdd/chapter3/ExpiryDateCalculator.java
+++ src/test/java/com/example/starttdd/chapter3/ExpiryDateCalculator.java
@@ -8,16 +8,23 @@ public class ExpiryDateCalculator {
     public LocalDate calculateExpiryDate(final PayData payData) {
         int addedMonths = payData.getPayAmount() / 10_000;
         if (payData.getFirstBillingDate() != null) {
-            final LocalDate candidateExp = payData.getBillingDate().plusMonths(addedMonths);
-            final int dayOfFirstBilling = payData.getFirstBillingDate().getDayOfMonth();
-            if (dayOfFirstBilling != payData.getBillingDate().getDayOfMonth()) {
-                final int dayLenOfCandiMon = YearMonth.from(candidateExp).lengthOfMonth();
-                if (dayLenOfCandiMon < dayOfFirstBilling) {
-                    return candidateExp.withDayOfMonth(dayLenOfCandiMon);
-                }
-                return candidateExp.withDayOfMonth(dayOfFirstBilling);
-            }
+            return expiryDateUsingFirstBillingDate(payData, addedMonths);
+        } else {
+            return payData.getBillingDate().plusMonths(addedMonths);
         }
-        return payData.getBillingDate().plusMonths(addedMonths);
+    }
+
+    private LocalDate expiryDateUsingFirstBillingDate(final PayData payData, final int addedMonths) {
+        final LocalDate candidateExp = payData.getBillingDate().plusMonths(addedMonths);
+        final int dayOfFirstBilling = payData.getFirstBillingDate().getDayOfMonth();
+        if (dayOfFirstBilling != payData.getBillingDate().getDayOfMonth()) {
+            final int dayLenOfCandiMon = YearMonth.from(candidateExp).lengthOfMonth();
+            if (dayLenOfCandiMon < dayOfFirstBilling) {
+                return candidateExp.withDayOfMonth(dayLenOfCandiMon);
+            }
+            return candidateExp.withDayOfMonth(dayOfFirstBilling);
+        } else {
+            return candidateExp;
+        }
     }
 }
