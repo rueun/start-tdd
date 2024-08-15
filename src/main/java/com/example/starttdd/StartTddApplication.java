9375ec9dfcb165de199dcc3744e3dcfe3dd19dc5
ejshin-muhayu <ejshin@muhayu.com>
2024-08-15 18:55:14 +0900

    init: 프로젝트 설정

---
+++ src/main/java/com/example/starttdd/StartTddApplication.java
@@ -0,0 +1,13 @@
+package com.example.starttdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartTddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StartTddApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/starttdd/StartTddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.starttdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartTddApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
