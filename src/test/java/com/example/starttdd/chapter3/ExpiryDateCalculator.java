4f4139026763f8203ec0c562a543ad7c370cc8df
ejshin-muhayu <ejshin@muhayu.com>
2024-08-15 19:54:49 +0900

    feat: 더할 일수 상수로 빼기

--- src/test/java/com/example/starttdd/chapter3/ExpiryDateCalculator.java
+++ src/test/java/com/example/starttdd/chapter3/ExpiryDateCalculator.java
@@ -5,12 +5,13 @@ import java.time.LocalDate;
 public class ExpiryDateCalculator {
 
     public LocalDate calculateExpiryDate(final PayData payData) {
+        int addedMonths = 1;
         if (payData.getFirstBillingDate() != null) {
-            final LocalDate candidateExp = payData.getBillingDate().plusMonths(1);
+            final LocalDate candidateExp = payData.getBillingDate().plusMonths(addedMonths);
             if (payData.getFirstBillingDate().getDayOfMonth() != payData.getBillingDate().getDayOfMonth()) {
                 return candidateExp.withDayOfMonth(payData.getFirstBillingDate().getDayOfMonth());
             }
         }
-        return payData.getBillingDate().plusMonths(1);
+        return payData.getBillingDate().plusMonths(addedMonths);
     }
 }
