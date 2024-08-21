c4be24de5e09e0ff601f76f063cb648cae126573
rueun <dkfah1229@naver.com>
2024-08-22 01:43:12 +0900

    Revert "test: assertEquals vs assertSame 테스트 코드 작성"
    This reverts commit 109e67006349b27314a896f90abdb5db81dac4b6.

--- src/test/java/com/example/starttdd/StartTddApplicationTests.java
+++ src/test/java/com/example/starttdd/StartTddApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.starttdd.chapter3;
+package com.example.starttdd;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
--- src/test/java/com/example/starttdd/chapter5/EqualTestRecord.java
+++
@@ -1,7 +0,0 @@
-package com.example.starttdd.chapter5;
-
-public record EqualTestRecord (int num, int num2) {
-    public EqualTestRecord update() {
-        return new EqualTestRecord(num, num2);
-    }
-}
