862300f9dd6e074a75b64f78a96ead57908ca6dc
rueun <dkfah1229@naver.com>
2024-08-22 10:24:23 +0900

    test: @DisplayNameTest 테스트

---
+++ src/test/java/com/example/starttdd/chapter5/DisplayNameTest.java
@@ -0,0 +1,30 @@
+package com.example.starttdd.chapter5;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("@DisplayName 테스트")
+class DisplayNameTest {
+
+    @DisplayName("값 같은지 비교")
+    @Test
+    void assertEqualsMethod() {
+
+    }
+
+    @Test
+    void failMethod() {
+
+    }
+
+    @DisplayName("익셉션 발생 여부 테스트")
+    @Test
+    void assertThrowsTest() {
+
+    }
+
+    @Test
+    void assertAllTest() {
+
+    }
+}
