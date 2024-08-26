4762e57982785833904bb962691e90d38b927d99
rueun <dkfah1229@naver.com>
2024-08-26 17:58:53 +0900

    feat: WeakPasswordChecker 및 stub 객체 추가

---
+++ src/test/java/com/example/starttdd/chapter7/testdouble/StubWeakPasswordChecker.java
@@ -0,0 +1,9 @@
+package com.example.starttdd.chapter7.testdouble;
+
+public class StubWeakPasswordChecker implements WeakPasswordChecker {
+    private boolean weak;
+
+    public void setWeak(boolean weak) {
+        this.weak = weak;
+    }
+}
