fb683eb9cbe3547399dd9b839cad73a984c7f401
rueun <dkfah1229@naver.com>
2024-08-27 22:12:03 +0900

    test: Mockito 를 이용한 모의 객체 생성과 사용

---
+++ src/test/java/com/example/starttdd/chapter7/testdouble/mock/UserRegisterMockTest.java
@@ -0,0 +1,38 @@
+package com.example.starttdd.chapter7.testdouble.mock;
+
+import com.example.starttdd.chapter7.testdouble.EmailNotifier;
+import com.example.starttdd.chapter7.testdouble.UserRegister;
+import com.example.starttdd.chapter7.testdouble.WeakPasswordChecker;
+import com.example.starttdd.chapter7.testdouble.WeakPasswordException;
+import com.example.starttdd.chapter7.testdouble.fake.MemoryUserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+
+class UserRegisterMockTest {
+    private UserRegister userRegister;
+    private WeakPasswordChecker mockPassWordChecker = mock(WeakPasswordChecker.class);
+    private MemoryUserRepository fakeRepository = new MemoryUserRepository();
+    private EmailNotifier mockEmailNotifier = mock(EmailNotifier.class);
+
+    @BeforeEach
+    void setUp() {
+        userRegister = new UserRegister(mockPassWordChecker, fakeRepository, mockEmailNotifier);
+    }
+
+    @DisplayName("약한 암호면 가입 실패")
+    @Test
+    void weakPassword() {
+        // mock 객체의 행동 설정
+        // checkPasswordWeak("pw")가 호출되면 true를 리턴하도록 설정
+        given(mockPassWordChecker.checkPasswordWeak("pw")).willReturn(true);
+
+        assertThrows(WeakPasswordException.class, () -> {
+            userRegister.register("id", "pw", "email");
+        });
+    }
+}
