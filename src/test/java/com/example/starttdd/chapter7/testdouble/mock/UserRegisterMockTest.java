9b2d82983f4e5bf0afa15b1a4fb0f12a52faa05c
rueun <dkfah1229@naver.com>
2024-08-27 22:23:36 +0900

    test: 모의 객체의 메서드를 호출할 때 전달한 인자를 구함

--- src/test/java/com/example/starttdd/chapter7/testdouble/mock/UserRegisterMockTest.java
+++ src/test/java/com/example/starttdd/chapter7/testdouble/mock/UserRegisterMockTest.java
@@ -8,7 +8,9 @@ import com.example.starttdd.chapter7.testdouble.fake.MemoryUserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
@@ -46,5 +48,19 @@ class UserRegisterMockTest {
         then(mockPassWordChecker)
                 .should(only())
                 .checkPasswordWeak("pw");
+    }
+
+    @DisplayName("가입하면 메일을 전송함")
+    @Test
+    void whenRegisterThenSendMail() {
+        userRegister.register("id", "pw", "email@email.com");
+
+        ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
+        then(mockEmailNotifier)
+                .should()
+                .sendRegisterEmail(captor.capture());
+
+        String realEmail = captor.getValue();
+        assertEquals("email@email.com", realEmail);
     }
 }
