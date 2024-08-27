eb10e65e354bb6dcb1511c5a696f3612c33c31da
rueun <dkfah1229@naver.com>
2024-08-27 20:15:50 +0900

    test: BDDMockito 를 이용한 스텁 설정

--- src/test/java/com/example/starttdd/supplement/c/GameGenMockTest.java
+++ src/test/java/com/example/starttdd/supplement/c/GameGenMockTest.java
@@ -1,12 +1,18 @@
 package com.example.starttdd.supplement.c;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
+import org.mockito.Mockito;
 
-import static org.mockito.Mockito.mock;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GameGenMockTest {
     @Test
     void mockTest() {
-        GameNumGen genMock = mock(GameNumGen.class);
+        GameNumGen genMock = Mockito.mock(GameNumGen.class);
+        BDDMockito.given(genMock.generate(new GameLevel())).willReturn("123");
+
+        String num = genMock.generate(new GameLevel());
+        assertEquals("123", num);
     }
 }
