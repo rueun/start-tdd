66f0112b234b3276280bc620e6937a3ec5fd7c2f
rueun <dkfah1229@naver.com>
2024-08-27 20:29:29 +0900

    test: 특정 타입의 익셉션을 발생하도록 스텁 설정

--- src/test/java/com/example/starttdd/supplement/c/GameGenMockTest.java
+++ src/test/java/com/example/starttdd/supplement/c/GameGenMockTest.java
@@ -5,14 +5,26 @@ import org.mockito.BDDMockito;
 import org.mockito.Mockito;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class GameGenMockTest {
     @Test
     void mockTest() {
+        // 1. Mock 객체 생성
         GameNumGen genMock = Mockito.mock(GameNumGen.class);
+        // 2. Mock 객체의 행동 설정(스텁 설정)
         BDDMockito.given(genMock.generate(GameLevel.EASY)).willReturn("123");
-
+        // 3. 스텁 설정에 매칭되는 메서드 호출
         String num = genMock.generate(GameLevel.EASY);
         assertEquals("123", num);
     }
+
+    @Test
+    void mockThrowTest() {
+        GameNumGen genMock = Mockito.mock(GameNumGen.class);
+        BDDMockito.given(genMock.generate(null)).willThrow(IllegalArgumentException.class);
+
+        assertThrows(IllegalArgumentException.class, () -> genMock.generate(null));
+    }
 }
+
